7220c715c782aaf8905ef2276797c98bd7290f11
Clarissa Calderon <clarissa.calderon9@gmail.com>
2023-05-27 11:50:47 -0700

    Fixed UI bug

--- Pseudo-Ku/BoardViewModel.swift
+++ Pseudo-Ku/BoardViewModel.swift
@@ -22,7 +22,7 @@ class BoardViewModel {
             let decoder = JSONDecoder()
             if let resultData = try? decoder.decode(ResultData.self, from: jsonData) {
                 self.resultData = resultData
-                completion(.success(resultData.board))
+                completion(.success(createMiniGrid()))
             } else {
                 completion(.failure(.parsingError))
             }
@@ -95,6 +95,46 @@ class BoardViewModel {
         }
         
         return !hasDuplicates
+    }
+    
+    func createMiniGrid() -> [[String]] {
+        var firstThreeColumns: [String] = []
+        var middleThreeColumns: [String] = []
+        var lastThreeColumns: [String] = []
+        
+        var gridByMiniSquares: [[String]] = []
+        
+        resultData.board.forEach { row in
+            firstThreeColumns.append(contentsOf: row.prefix(3))
+            middleThreeColumns.append(contentsOf: row.dropFirst(3).prefix(3))
+            lastThreeColumns.append(contentsOf: row.dropFirst(6).prefix(3))
+        }
+        
+        var miniGrid1 = Array(firstThreeColumns.prefix(9))
+        var miniGrid2 = Array(middleThreeColumns.prefix(9))
+        var miniGrid3 = Array(lastThreeColumns.prefix(9))
+        
+        var miniGrid4 = Array(firstThreeColumns.dropFirst(9).prefix(9))
+        var miniGrid5 = Array(middleThreeColumns.dropFirst(9).prefix(9))
+        var miniGrid6 = Array(lastThreeColumns.dropFirst(9).prefix(9))
+        
+        var miniGrid7 = Array(firstThreeColumns.dropFirst(18).prefix(9))
+        var miniGrid8 = Array(middleThreeColumns.dropFirst(18))
+        var miniGrid9 = Array(lastThreeColumns.dropFirst(18))
+        
+        gridByMiniSquares.append(miniGrid1)
+        gridByMiniSquares.append(miniGrid2)
+        gridByMiniSquares.append(miniGrid3)
+        
+        gridByMiniSquares.append(miniGrid4)
+        gridByMiniSquares.append(miniGrid5)
+        gridByMiniSquares.append(miniGrid6)
+        
+        gridByMiniSquares.append(miniGrid7)
+        gridByMiniSquares.append(miniGrid8)
+        gridByMiniSquares.append(miniGrid9)
+        
+        return gridByMiniSquares
     }
 }
 
